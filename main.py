53cbbbab1bb176f4d69fdd7f08c7554ee035efae
CitizenFourCF4 <const.orel@mail.ru>
2023-08-28 11:46:02 +0300

    FAST FIX

--- main.py
+++ main.py
@@ -6,4 +6,4 @@ bot = telebot.TeleBot(token)
 
 if __name__ == '__main__':
     pr_title, contributor_login, time_of_applying_changes, repo_name = functions.parse_command_line_args()
-    bot.send_message(chat_id=453041569, text=f"*{pr_title}*\n\n{contributor_login}\n{time_of_applying_changes}\n{repo_name}", parse_mode='Markdown')
+    bot.send_message(chat_id=453041569, text=f"*UPDATE ⚙️*\n\n{pr_title}\n\n{contributor_login}\n{time_of_applying_changes}\n{repo_name}", parse_mode='Markdown')
