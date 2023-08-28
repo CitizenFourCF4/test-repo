68727d77f5d4fb0a419973404b21bc988625b9d4
CitizenFourCF4 <94146075+CitizenFourCF4@users.noreply.github.com>
2023-08-28 16:57:41 +0300

    Update test.sh

--- .github/workflows/test.sh
+++ .github/workflows/test.sh
@@ -15,10 +15,14 @@ PARSE_MODE="Markdown"
 HEADER="*UPDATE ⚙️*"
 WHAT_CHANGED="*WHAT CHANGED?*"
 
+BUTTON_URL="$PR_LINK"
+BUTTON_TEXT="Открыть"
+BUTTON='[{"text": "'$BUTTON_TEXT'", "url": "'$BUTTON_URL'"}]'
+
 if [ -z "$DETAILS" ]; then
    DETAILS="Developer left no comments"
 fi
 
 MESSAGE="$HEADER%0A%0A$PR_TITLE%0A%0A$WHAT_CHANGED%0A$DETAILS%0A%0A_repository name_| $REPOSITORY_NAME%0A_developer_| $DEVELOPER_LOGIN"
 
-curl -s -X POST "https://api.telegram.org/bot$BOT_API_KEY/sendMessage" -d chat_id=$CHAT_ID -d text="$MESSAGE" -d parse_mode="$PARSE_MODE"
+curl -s -X POST "https://api.telegram.org/bot$BOT_API_KEY/sendMessage" -d chat_id=$CHAT_ID -d text="$MESSAGE" -d parse_mode="$PARSE_MODE", reply_markup={"inline_keyboard": ['$BUTTON']}
