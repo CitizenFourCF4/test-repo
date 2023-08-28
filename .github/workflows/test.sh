e712a9c6a2d4b4784ae0417f7da140f393a5cd3c
CitizenFourCF4 <94146075+CitizenFourCF4@users.noreply.github.com>
2023-08-28 16:24:17 +0300

    Update test.sh

--- .github/workflows/test.sh
+++ .github/workflows/test.sh
@@ -8,22 +8,17 @@ do
    export "$KEY"="$VALUE"
 done
 
-# use here your expected variables
-echo "PR_TITLE = $PR_TITLE"
-echo "REPOSITORY_NAME = $REPOSITORY_NAME"
-echo "DETAILS = $DETAILS"
-echo "DEVELOPER_LOGIN = $DEVELOPER_LOGIN"
-
 BOT_API_KEY="6634635160:AAG2A8EHdfRafEOZUsKgpEjAaz8uWy0yjYg"
 CHAT_ID="453041569"
 PARSE_MODE="Markdown"
 
 HEADER="*UPDATE ⚙️*"
+WHAT_WAS_ADDED="_*WHAT WAS ADDED?*_"
 
 if [ -z "$DETAILS" ]; then
    DETAILS="Developer left no comments"
 fi
 
-MESSAGE="$HEADER%0A%0A$PR_TITLE%0A%0A_repository name_| $REPOSITORY_NAME%0A_developer_| $DEVELOPER_LOGIN%0A_details_|%0A $DETAILS"
+MESSAGE="$HEADER%0A%0A$PR_TITLE%0A%0A$WHAT_WAS_ADDED%0A$DETAILS%0A%0A_repository name_| $REPOSITORY_NAME%0A_developer_| $DEVELOPER_LOGIN"
 curl -s -X POST "https://api.telegram.org/bot$BOT_API_KEY/sendMessage" -d chat_id=$CHAT_ID -d text="$MESSAGE" -d parse_mode="$PARSE_MODE"
 
