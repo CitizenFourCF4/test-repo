9f999c2623dea7c8e44991f0e5acdeab62019ecf
CitizenFourCF4 <94146075+CitizenFourCF4@users.noreply.github.com>
2023-08-28 20:44:57 +0300

    Update update_notifier.sh

--- .github/workflows/update_notifier.sh
+++ .github/workflows/update_notifier.sh
@@ -15,5 +15,5 @@ if [ -z "$DETAILS" ]; then
    DETAILS="Developer left no comments"
 fi
 
-MESSAGE="$HEADER%0A%0A$PR_TITLE%0A%0A$WHAT_CHANGED%0A$DETAILS%0A%0A_repository name_| $REPOSITORY_NAME%0A_developer_| $DEVELOPER_LOGIN%0A_link_| $PR_LINK"
+MESSAGE="$HEADER%0A%0A$PR_TITLE%0A%0A$WHAT_CHANGED%0A$DETAILS%0A%0A`repository name`| $REPOSITORY_NAME%0A`developer`| $DEVELOPER_LOGIN%0A`link`| $PR_LINK"
 curl -s -X POST "https://api.telegram.org/bot$BOT_API_KEY/sendMessage" -d chat_id="$CHAT_ID" -d text="$MESSAGE" -d parse_mode="$PARSE_MODE"
